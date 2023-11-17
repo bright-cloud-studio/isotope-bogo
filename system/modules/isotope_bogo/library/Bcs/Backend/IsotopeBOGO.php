27f32d29699b3519c9ca85f1be3983fa449a47f2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-17 14:38:41 -0500

    Update IsotopeBOGO.php

--- system/modules/isotope_bogo/library/Bcs/Backend/IsotopeBOGO.php
+++ system/modules/isotope_bogo/library/Bcs/Backend/IsotopeBOGO.php
@@ -20,28 +20,27 @@ use Isotope\Model\ProductCollection;
 use Isotope\Model\ProductCollection\Cart;
 use Isotope\Model\ProductCollection\Order;
 
-
-
 class IsotopeBOGO extends System {
 
     
     /* HOOK - Triggered when trying to add a product to the cart on a Product Reader page */
+    // RETURN PRODUCT QUANTITY
     public function addItemToCollection( Product $objProduct, $intQuantity, IsotopeProductCollection $objCollection ){
-
+        
         // System Log Message
         \Controller::log('BOGO: checkCollectionQuantity Triggered', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
-
-        return false;
+        
+        return $intQuantity;
     }
-    
     
     /* HOOK - Triggered when trying to update our quantity on a Cart page */
     public function updateItemInCollection($objItem, $arrSet, $objCart) {
 
         // System Log Message
         \Controller::log('BOGO: updateCollectionQuantity Triggered', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+        
+        return $arrSet;
     }
-
     
     /* HOOK - Triggered when two carts have merged together (when a guest logs in while having items in their cart, while their account already had a cart attached to it */
     public function mergeWithGuestCollection(IsotopeProductCollection $oldCollection, IsotopeProductCollection $newCollection)
@@ -49,6 +48,7 @@ class IsotopeBOGO extends System {
         // System Log Message
         \Controller::log('BOGO: mergeCollections Triggered', __CLASS__ . '::' . __FUNCTION__, 'GENERAL'); 
         
+        return true;
     }
   
 }
