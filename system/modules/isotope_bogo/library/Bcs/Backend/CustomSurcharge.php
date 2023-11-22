dfed9ccc1b3d1b7624352683689a2e25cb5ea039
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-22 11:32:53 -0500

    Update CustomSurcharge.php

--- system/modules/isotope_bogo/library/Bcs/Backend/CustomSurcharge.php
+++ system/modules/isotope_bogo/library/Bcs/Backend/CustomSurcharge.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * Bright Cloud Studio's Isotope BOGO
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-bogo
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+
+namespace Bcs\Backend;
+
 use Isotope\Model\ProductCollection;
 
 class CustomSurcharge
