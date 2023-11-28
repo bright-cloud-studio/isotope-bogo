be6c47276cddd5c25a67d63fe66a4fbb8b4a3e71
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-28 13:16:53 -0500

    Update MySurchargeModel.php

--- system/modules/isotope_bogo/library/Bcs/Model/MySurchargeModel.php
+++ system/modules/isotope_bogo/library/Bcs/Model/MySurchargeModel.php
@@ -12,7 +12,7 @@ class MySurchargeModel extends ProductCollectionSurcharge implements IsotopeProd
         $surcharge = new self();
         $surcharge->label = 'My Surcharge';
         $surcharge->price = '';
-        $surcharge->total_price = 100.0;
+        $surcharge->total_price = -5.0;
         $surcharge->tax_class = 1;
         $surcharge->before_tax = true;
         $surcharge->addToTotal = true;
