8c6c9275299e1af3f84083f43aef84f127eb14d9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-30 13:22:28 -0500

    Create default.php

---
+++ system/modules/isotope_bogo/languages/en/default.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Bright Cloud Studio's Isotope BOGO
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-bogo
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+/* Attributes */
+$GLOBALS['TL_LANG']['ATTR']['bogo']      = array('Buy One, Get One Settings', 'Holds the settings used for BOGO.');
