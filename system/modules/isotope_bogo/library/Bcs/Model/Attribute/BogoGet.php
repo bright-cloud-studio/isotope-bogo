358a8bc19a48b8c86566aa6c54547c3ad629398d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-30 13:40:54 -0500

    Create BogoGet.php

---
+++ system/modules/isotope_bogo/library/Bcs/Model/Attribute/BogoGet.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Isotope eCommerce for Contao Open Source CMS
+ *
+ * Copyright (C) 2009-2014 terminal42 gmbh & Isotope eCommerce Workgroup
+ *
+ * @package    Isotope
+ * @link       http://isotopeecommerce.org
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+namespace Bcs\Model\Attribute;
+
+use Isotope\Interfaces\IsotopeAttribute;
+use Isotope\Interfaces\IsotopeProduct;
+use Isotope\Model\Attribute;
+
+
+/**
+ * Attribute to provide an audio/video player in the product details
+ *
+ * @copyright  Isotope eCommerce Workgroup 2009-2014
+ * @author     Christoph Wiechert <cw@4wardmedia.de>
+ */
+class BogoGet extends Attribute implements IsotopeAttribute
+{
+    /**
+     * Return class name for the backend widget or false if none should be available
+     * @return    string
+     */
+    public function getBackendWidget()
+    {
+        return $GLOBALS['BE_FFL']['text'];
+    }
+
+
+    /**
+     * Generate youtube attribute
+     *
+     * @param \Isotope\Interfaces\IsotopeProduct $objProduct
+     * @param array $arrOptions
+     * @return string
+     */
+    public function generate(IsotopeProduct $objProduct, array $arrOptions = array())
+    {
+        
+    }
+}
