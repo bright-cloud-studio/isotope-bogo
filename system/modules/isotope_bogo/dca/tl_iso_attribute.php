45b266a7f778740c5e730b05e2886dc65a7a591b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-30 13:37:50 -0500

    Update tl_iso_attribute.php

--- system/modules/isotope_bogo/dca/tl_iso_attribute.php
+++ system/modules/isotope_bogo/dca/tl_iso_attribute.php
@@ -9,33 +9,6 @@
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 */
 
- 
- 
-/**
- * Load tl_iso_product language file for field legends
- */
-\System::loadLanguageFile('tl_iso_product');
-
-
 // Palettes
-$GLOBALS['TL_DCA']['tl_iso_attribute']['palettes']['bogo'] = '{attribute_legend},name,field_name,type,legend;{bogo_legend},bogo_buy,bogo_get;';
-
-// Fields
-$GLOBALS['TL_DCA']['tl_iso_attribute']['fields']['bogo_buy'] = array
-(
-  'label'                   => &$GLOBALS['TL_LANG']['tl_iso_attribute']['bogo_buy'],
-  'inputType'               => 'text',
-  'default'                 => '0',
-  'search'                  => true,
-  'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
-  'sql'                     => "varchar(255) NOT NULL default ''",
-);
-$GLOBALS['TL_DCA']['tl_iso_attribute']['fields']['bogo_get'] = array
-(
-  'label'                   => &$GLOBALS['TL_LANG']['tl_iso_attribute']['bogo_get'],
-  'inputType'               => 'text',
-  'default'                 => '0',
-  'search'                  => true,
-  'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
-  'sql'                     => "varchar(255) NOT NULL default ''",
-);
+$GLOBALS['TL_DCA']['tl_iso_attribute']['palettes']['bogo_buy'] = '{attribute_legend},name,field_name,type,legend;{description_legend:hide},iso_description;{config_legend},mandatory';
+$GLOBALS['TL_DCA']['tl_iso_attribute']['palettes']['bogo_get'] = '{attribute_legend},name,field_name,type,legend;{description_legend:hide},iso_description;{config_legend},mandatory';
