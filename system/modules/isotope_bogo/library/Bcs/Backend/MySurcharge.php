a14059a91b512e48cbf84e23bfd09044d20d38d6
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-30 10:57:24 -0500

    Update MySurcharge.php

--- system/modules/isotope_bogo/library/Bcs/Backend/MySurcharge.php
+++ system/modules/isotope_bogo/library/Bcs/Backend/MySurcharge.php
@@ -2,13 +2,37 @@
 
 namespace Bcs\Backend;
 
+use Isotope\Interfaces\IsotopeProductCollection;
+
 use Isotope\Model\ProductCollection;
 use Bcs\Model\MySurchargeModel;
 
 class MySurcharge
 {
-    public function findSurchargesForCollection(ProductCollection $collection): array
+    public function findSurchargesForCollection(IsotopeProductCollection $collection): array
     {
-        return [MySurchargeModel::build()];
+        
+        // the total amount to discount from our cart
+        $free_discount = 0;
+        
+        
+        foreach($collection->getItems() as $oItem) {
+            //echo "Quantity: " . $oItem->quantity . '<br>';
+            //echo "Quantity (Free): " . $oItem->quantity_free . '<br>';
+            //echo 'Price: ' . $oItem->price;
+            
+            $free_discount += $oItem->price * $oItem->quantity_free;
+        }
+        
+        
+        $bogo = new MySurchargeModel;
+        
+        $bogo->label = "Two Free!";
+        $bogo->total_price = $free_discount * (-1);
+        $bogo->tax_class = 1;
+        $bogo->before_tax = true;
+        $bogo->addToTotal = true;
+        
+        return [$bogo];
     }
 }
