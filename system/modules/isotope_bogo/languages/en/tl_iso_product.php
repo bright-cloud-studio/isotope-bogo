9d00c28d635bc807486baa1d9a90fff6d7a9cc52
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-30 11:40:21 -0500

    Create tl_iso_product.php

---
+++ system/modules/isotope_bogo/languages/en/tl_iso_product.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Bright Cloud Studio's Isotope BOGO
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-bogo
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+*/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_iso_product']['bogo_buy_limit']   = array('When you buy X', 'For every X of this product you buy...');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_iso_product']['bogo_legend'] = 'Buy One, Get One Settings';
