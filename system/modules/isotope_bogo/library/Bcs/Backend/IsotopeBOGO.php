965c884775b8b9440272a59bd6462c049f2208ee
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-04 13:50:49 -0500

    Update IsotopeBOGO.php

--- system/modules/isotope_bogo/library/Bcs/Backend/IsotopeBOGO.php
+++ system/modules/isotope_bogo/library/Bcs/Backend/IsotopeBOGO.php
@@ -54,7 +54,7 @@ class IsotopeBOGO extends System {
     
     
     /* HOOK - When a quantity is updated on a Cart page */
-    public function updateItemInCollection($objItem, $arrSet, $objCart) {
+    public function postUpdateItemInCollection($objItem, $arrSet, $objCart) {
         
         \Controller::log('BOGO: Hook 2', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
 
